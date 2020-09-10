b1a52c2114c91b5c5e34501c140d206ef7476021
yichunzhao <zhaoyichun@hotmail.com>
2020-09-10 22:15:31 +0200

    consuming services via Feign interace

--- src/main/java/com/ynz/democloud/roomreservationservice/RoomReservationWebService.java
+++ src/main/java/com/ynz/democloud/roomreservationservice/RoomReservationWebService.java
@@ -1,26 +1,23 @@
 package com.ynz.democloud.roomreservationservice;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("roomreservations")
+@RequestMapping("/room-reservations")
 @RequiredArgsConstructor
 public class RoomReservationWebService {
-    private final RestTemplate restTemplate;
+    private final RoomClient roomClient;
 
     @GetMapping
     public ResponseEntity<List<RoomReservation>> getRoomReservations() {
-        List<Room> rooms = getAllRooms();
+        List<Room> rooms = roomClient.getAllRooms();
         List<RoomReservation> reservations = new ArrayList<>();
 
         rooms.forEach(room -> {
@@ -33,13 +30,5 @@ public class RoomReservationWebService {
         return ResponseEntity.ok(reservations);
     }
 
-    private List<Room> getAllRooms() {
-
-        ResponseEntity<List<Room>> responseEntity = restTemplate.exchange("http://ROOMSERVICES/rooms",
-                HttpMethod.GET, null, new ParameterizedTypeReference<List<Room>>() {
-                }
-        );
-        return responseEntity.getBody();
-    }
 
 }
